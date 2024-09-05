bb9661a985635d26ac01ba152fe270ddc32c253e
Jhonny Stiven Agudelo Tenorio <jhonny.agudelo3120@gmail.com>
2024-09-05 14:55:57 -0500

    se termina el curso

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,40 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 
 const blogCollection = defineCollection({
   type: 'content',
-  schema: ({image})=> z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     date: z.date(),
     description: z.string(),
-    image: image().refine((img) => img.width < 1200,{
-      message:'Image should be greater than 1200px'
+    image: image().refine((img) => img.width < 1200, {
+      message: 'Image should be greater than 1200px'
     }),
     // relacion
-    author: z.string(),
+    // author: z.string(),
+    author: reference('author'),
     // relacion
     tags: z.array(z.string()),
+    //Boolean
+    isDraft: z.boolean().default(false)
+  })
+})
+
+const authorCollection = defineCollection({
+  type: 'data',
+  schema: ({ image }) => z.object({
+    name: z.string(),
+    avatar: image().refine((img) => img.width < 1200, {
+      message: 'Image should be greater than 1200px'
+    }),
+    twitter: z.string(),
+    linkedIn: z.string(),
+    github: z.string(),
+    bio: z.string(),
+    subtitle: z.string()
   })
 })
 
 export const collections = {
-  'blog': blogCollection
+  'blog': blogCollection,
+  'author': authorCollection,
 }
